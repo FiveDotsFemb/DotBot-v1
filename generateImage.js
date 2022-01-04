93ae6c9d85264a933d4e8c8ed5933f5dc45c7722
FiveDotsFemb <ritarirasmus@gmail.com>
2022-01-04 22:48:53 +0200

    testing

--- generateImage.js
+++ generateImage.js
@@ -1,7 +1,7 @@
 const Canvas = require("canvas")
 const Discord = require("discord.js")
 
-const background = "https://i.imgur.com/0crWzip.png"
+const background = (process.env.IMAGE)
 
 const dim = {
     height: 675,
