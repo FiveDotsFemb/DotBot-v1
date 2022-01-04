aefa850834d2e9e01df60982a74ebfec74d7578a
FiveDotsFemb <ritarirasmus@gmail.com>
2022-01-04 22:42:42 +0200

    bot

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
+const generateImage = require("./generateImage")
+
 const PREFIX = "dot!"
 
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
@@ -14,10 +17,33 @@ client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
 })
 
-client.on("messageCreate", (message) => {
-    if (message.content == "hi" ){
-        message.reply("hello")
+client.on('message', msg => {
+    if (!msg.content.startsWith(PREFIX)) return;
+
+    const args = msg.content.slice(PREFIX.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+
+    if (command === 'avatar') {
+        const embed = new Discord.MessageEmbed()
+            .setTitle('User Avatar')
+            .setColor('#0099ff')
+            .setThumbnail(msg.author.avatarURL())
+            .setImage(msg.author.avatarURL())
+            .setTimestamp()
+        msg.channel.send({embeds: [embed]});
+            
     }
+})
+
+const welcomeChannelId = "896513423863599116"
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}> Welcome to the server!`,
+        files: [img]
+    })
 })
 
 client.login(process.env.TOKEN)
